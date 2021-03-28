0a8ee297145aa5c48b8130022435983fba46798b
biagavirete <biagavirete@gmail.com>
2021-03-28 15:05:44 -0300

    feat: create components

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,16 +3,18 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   :root {
     --red: #E52E4D;
+    --green: #33CC95;
     --blue: #5429CC;
 
     --blue-light: #6933FF;
 
-    -text-title: #363F5F;
-    -text-body: #969CB3;
+    --text-title: #363F5F;
+    --text-body: #969CB3;
 
     --background: #F2F0F5;
     --shape: #FFFFFF;
   }
+  
   * {
     margin: 0;
     padding: 0;
